5924501faa6ad5769641dee518840d8150044a51
Marcos <marcoscruz1992@gmail.com>
2014-09-27 22:43:09 -0400

    minor button styling and description validation

--- app/models/confession.rb
+++ app/models/confession.rb
@@ -2,5 +2,6 @@ class Confession < ActiveRecord::Base
 	belongs_to :user
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
     validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+    validates :description, presence: true
 
 end
